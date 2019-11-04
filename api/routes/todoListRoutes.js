1674bda0929c9d66cbbc805ef0d8b2955606a073
Toseef Zafar <mtouseef.zafar@gmail.com>
2019-11-04 23:01:15 +0000

    Simplified routes

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,15 +1,28 @@
 'use strict';
 module.exports = function (app) {
-  var todoList = require('../controllers/todoListController');
+    var todoList = require('../controllers/todoListController');
 
-  // todoList Routes
-  app.route('/tasks')
-      .get(todoList.list_all_tasks)
-      .post(todoList.create_a_task);
+    //Note: these below also work
+    // todoList Routes
+    // app.route('/tasks')
+    //     .get(todoList.list_all_tasks);
+    //.post(todoList.create_a_task);
 
 
-  app.route('/tasks/:taskId')
-      .get(todoList.read_a_task)
-      .put(todoList.update_a_task)
-      .delete(todoList.delete_a_task);
+    // app.route('/tasks/:taskId')
+    //     .get(todoList.read_a_task)
+    //     .put(todoList.update_a_task)
+    //     .delete(todoList.delete_a_task);
+
+
+    //simplified form
+    app.get('/tasks', todoList.list_all_tasks);
+
+    app.get('/tasks/:taskId', [todoList.read_a_task]);
+
+    app.post('/tasks', [todoList.create_a_task]);
+
+    app.put('/tasks/:taskId', [todoList.update_a_task]);
+
+    app.delete('/tasks/:taskId', [todoList.delete_a_task]);
 };
