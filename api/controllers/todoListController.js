173529daefa302a86dfd5972c22f30b9fe080fe3
Toseef Zafar <mtouseef.zafar@gmail.com>
2019-11-04 22:48:47 +0000

    simplified controller functions.

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -1,66 +1,35 @@
 'use strict';
 
+function send_response_back(res) {
+    return function (err, task) {
+        if (err)
+            res.send(err);
+        res.json(task);
+    };
+}
 
-var mongoose = require('mongoose'),
-    Task = mongoose.model('Tasks');
+var mongoose = require('mongoose');
+var Task = mongoose.model('Tasks');
 
 exports.list_all_tasks = function (req, res) {
-    function list_tasks() {
-        return function (err, task) {
-            if (err)
-                res.send(err);
-            res.json(task);
-        };
-    }
-
-    Task.find({}, list_tasks(req, res));
+    Task.find({}, send_response_back(res));
 };
-
 
 exports.create_a_task = function (req, res) {
     var new_task = new Task(req.body);
-
-    function create_task() {
-        return function (err, task) {
-            if (err)
-                res.send(err);
-            res.json(task);
-        };
-    }
-
-    new_task.save(create_task(req, res));
+    new_task.save(send_response_back(res));
 };
 
-
 exports.read_a_task = function (req, res) {
-    function read_task() {
-        return function (err, task) {
-            if (err)
-                res.send(err);
-            res.json(task);
-        };
-    }
-
-    Task.findById(req.params.taskId, read_task(req, res));
+    Task.findById(req.params.taskId, send_response_back(res));
 };
 
 
 exports.update_a_task = function (req, res) {
-    function update_task() {
-        return function (err, task) {
-            if (err)
-                res.send(err);
-            res.json(task);
-        };
-    }
-
-    Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, update_task(req, res));
+    Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, send_response_back(res));
 };
 
-
 exports.delete_a_task = function (req, res) {
-
-
     function delete_task() {
         return function (err, task) {
             if (err)
@@ -69,7 +38,5 @@ exports.delete_a_task = function (req, res) {
         };
     }
 
-    Task.remove({
-        _id: req.params.taskId
-    }, delete_task(req, res));
+    Task.remove({_id: req.params.taskId}, delete_task(req, res));
 };
